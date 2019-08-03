b254c93c0689bc4e73ddad337187584ccd0e7670
fengandzhy <fengandzhy@gmail.com>
2019-08-04 10:52:32 +1200

    dispatch to index

---
+++ spring-mvc-demos/springmvc-demo01/src/main/java/com/citi/springmvc/controller/IndexController.java
@@ -0,0 +1,15 @@
+package com.citi.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value="/")
+	public String index(){
+		
+		//返回对应的视图名字
+		return "index";
+	}
+}
