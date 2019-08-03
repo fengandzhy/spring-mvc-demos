4e06c9d21f3e69701898392f5c8ffefd877cb4ad
fengandzhy <fengandzhy@gmail.com>
2019-08-04 11:19:45 +1200

    parameters for @RequestMappinig

--- spring-mvc-demos/springmvc-demo01/src/main/java/com/citi/springmvc/controller/IndexController.java
+++ spring-mvc-demos/springmvc-demo01/src/main/java/com/citi/springmvc/controller/IndexController.java
@@ -3,6 +3,7 @@ package com.citi.springmvc.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/")
 @Controller
 public class IndexController {
 	
@@ -12,4 +13,10 @@ public class IndexController {
 		//返回对应的视图名字
 		return "index";
 	}
+	
+	public String view(){
+		return null;
+	}
+	
+	
 }
