f746fb8c123e0a4e4c69350e23023396fc2c89e3
fengandzhy <fengandzhy@gmail.com>
2019-08-04 22:36:04 +1200

    another style for implements springmvc

---
+++ spring-mvc-demos/springmvc-dispatcherservler/src/main/java/com/citi/springmvc/controller/HelloContorller.java
@@ -0,0 +1,18 @@
+package com.citi.springmvc.controller;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class HelloContorller implements Controller {
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		mv.setViewName("/index.jsp");
+		return mv;
+	}
+
+}
