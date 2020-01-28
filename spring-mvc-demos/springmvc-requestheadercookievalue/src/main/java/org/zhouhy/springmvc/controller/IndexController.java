57aa8f0fe1e886cd446ae1bd05b77ccf2d326fc3
fengandzhy <34152012+fengandzhy@users.noreply.github.com>
2020-01-28 21:37:48 +1300

    new project

---
+++ spring-mvc-demos/springmvc-requestheadercookievalue/src/main/java/org/zhouhy/springmvc/controller/IndexController.java
@@ -0,0 +1,14 @@
+package org.zhouhy.springmvc.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@RequestMapping(value="/")
+@Controller
+public class IndexController {
+	
+	@RequestMapping(value="/")
+	public String index(){
+		return "index";
+	}
+}
