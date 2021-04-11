ae1a6dfd500f133e0bd051673291ffe945af0a22
fengandzhy <fengandzhy@gmail.com>
2021-04-12 04:58:10 +1200

    remove

---
+++ springmvc-demos/spring-mvc-01/src/main/java/org/zhouhy/springmvc/controller/test.java
@@ -0,0 +1,8 @@
+package org.zhouhy.springmvc.controller;
+
+public class test {
+    public static void main(String[] args) {
+        String str = "hello 李宁";
+        System.out.println(str.length());
+    }
+}
