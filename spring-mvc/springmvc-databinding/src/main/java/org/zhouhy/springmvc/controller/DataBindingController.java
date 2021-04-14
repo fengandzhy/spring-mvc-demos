147c54377b1e8605ef58447057e7c3e771e96be8
fengandzhy <fengandzhy@gmail.com>
2021-04-15 09:43:43 +1200

    remove the unuse project

--- spring-mvc/springmvc-databinding/src/main/java/org/zhouhy/springmvc/controller/DataBindingController.java
+++ spring-mvc/springmvc-databinding/src/main/java/org/zhouhy/springmvc/controller/DataBindingController.java
@@ -2,9 +2,11 @@ package org.zhouhy.springmvc.controller;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.format.annotation.DateTimeFormat;
-import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.zhouhy.springmvc.editor.SpecialDateEditor;
 
 import java.util.Date;
 
@@ -13,8 +15,7 @@ import java.util.Date;
 public class DataBindingController {
     
     @RequestMapping("/bindingDate1")
-    public String bindingDate(@DateTimeFormat(iso=DateTimeFormat.ISO.DATE) Date date,
-                            @DateTimeFormat(pattern = "yyyy-MM-dd HH:mm:ss")Date dateTime){
+    public String bindingDate(Date date,Date dateTime){
         log.info(date.toString());
         log.info(dateTime.toString());
         return "success";
@@ -25,5 +26,10 @@ public class DataBindingController {
         log.info(date);
         log.info(dateTime);
         return "success";
+    }
+
+    @InitBinder
+    public void initBinder(WebDataBinder b) {
+        b.registerCustomEditor(Date.class, new SpecialDateEditor());
     }
 }
---
+++ spring-mvc/springmvc-databinding/src/main/java/org/zhouhy/springmvc/editor/SpecialDateEditor.java
@@ -0,0 +1,76 @@
+package org.zhouhy.springmvc.editor;
+
+
+
+import com.sun.corba.se.impl.io.TypeMismatchException;
+import org.apache.commons.lang3.StringUtils;
+
+
+import java.beans.PropertyEditorSupport;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Locale;
+import java.util.regex.Pattern;
+
+public class SpecialDateEditor extends PropertyEditorSupport {
+
+    @Override
+    public void setAsText(String text) throws IllegalArgumentException {
+        Date date = null;
+        SimpleDateFormat sdf = null;
+
+        try {
+            if (Pattern.compile("([GMT]|[gmt])").matcher(text).find()) { //Wed Nov 21 2018 08:00:00 GMT+0800(中国标准时间)
+                sdf = new SimpleDateFormat("EEE MMM dd yyyy HH:mm:ss z", Locale.ENGLISH);
+                try {
+                    text = text.replace("GMT", "").replaceAll("\\(.*\\)", "");
+                    date = sdf.parse(text);
+                    setValue(date);
+                    return;
+                } catch (ParseException e) {
+                    // TODO Auto-generated catch block
+                    e.printStackTrace();
+                }
+            }
+            sdf = getSimpleDateFormat(text);
+            //防止空数据出错
+            if(StringUtils.isNotBlank(text)){
+                date = sdf.parse(text);
+            }
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+        setValue(date);
+    }
+
+
+    private SimpleDateFormat getSimpleDateFormat(String source) {
+        SimpleDateFormat sdf = new SimpleDateFormat();
+        if (Pattern.matches("^\\d{4}-\\d{2}-\\d{2}$", source)) { // yyyy-MM-dd
+            sdf = new SimpleDateFormat("yyyy-MM-dd");
+        } else if (Pattern.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}-\\d{2}-\\d{2}$", source)) { // yyyy-MM-dd HH-mm-ss
+            sdf = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+        } else if (Pattern.matches("^\\d{4}-\\d{2}-\\d{2} \\d{2}:\\d{2}:\\d{2}$", source)) { // yyyy-MM-dd HH:mm:ss
+            sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+        } else if (Pattern.matches("^\\d{4}/\\d{2}/\\d{2}$", source)) { // yyyy/MM/dd
+            sdf = new SimpleDateFormat("yyyy/MM/dd");
+        } else if (Pattern.matches("^\\d{4}/\\d{2}/\\d{2} \\d{2}/\\d{2}/\\d{2}$", source)) { // yyyy/MM/dd HH/mm/ss
+            sdf = new SimpleDateFormat("yyyy/MM/dd HH/mm/ss");
+        }  else if (Pattern.matches("^\\d{4}\\d{2}\\d{2}$", source)) { // yyyyMMdd
+            sdf = new SimpleDateFormat("yyyyMMdd");
+        }  else if (Pattern.matches("^\\d{4}\\d{2}\\d{2} \\d{2}\\d{2}\\d{2}$", source)) { // yyyyMMdd HHmmss
+            sdf = new SimpleDateFormat("yyyyMMdd HHmmss");
+        } else if (Pattern.matches("^\\d{4}\\.\\d{2}\\.\\d{2}$", source)) { // yyyy.MM.dd
+            sdf = new SimpleDateFormat("yyyy.MM.dd");
+        }  else if (Pattern.matches("^\\d{4}\\.\\d{2}\\.\\d{2} \\d{2}\\.\\d{2}\\.\\d{2}$", source)) { // yyyy.MM.dd HH.mm.ss
+            sdf = new SimpleDateFormat("yyyy.MM.dd HH.mm.ss");
+        }else if (Pattern.matches("^\\d{4}-\\d{2}$", source)) { // yyyy-MM-dd
+            sdf = new SimpleDateFormat("yyyy-MM");
+        }else{
+            System.out.println("TypeMismatchException");
+            throw new TypeMismatchException();
+        }
+        return sdf;
+    }   
+}
